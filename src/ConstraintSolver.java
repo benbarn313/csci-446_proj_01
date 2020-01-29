6808a3e24ef794e4ebaf3d1a46fcf3f0b8a405e2
Ben P <polk.ben@gmail.com>
2020-01-28 23:25:53 -0700

    Add simple backtracking algorithm

--- src/ConstraintSolver.java
+++ src/ConstraintSolver.java
@@ -65,4 +65,92 @@ public class ConstraintSolver
         return true;
     }
 
+    //***************************************************
+    //*********Code for Simple Backtracking**************
+    public void simpleBacktrack(Graph myGraph, int numColors)
+    {
+        prepare(myGraph.getNumNodes(), numColors);
+
+        simpleBTColor(getNextNode(myGraph), myGraph);
+
+        if (!hasSolution(myGraph)) cost *= -1;
+    }
+
+    private void simpleBTColor(int curNode, Graph myGraph)
+    {
+        cost++;
+        boolean foundColor = false;
+
+        //loop through color assignments to the node, attempting to find a valid one
+        int i = 1;
+        while (!foundColor && i <= colorVals.length - 1)
+        {
+            cost++;
+
+            //check that this coloring is valid
+            if (!causesConflicts(myGraph, curNode, colorVals[i]))
+            {
+                nodeColors[curNode] = colorVals[i];
+                int nextNode = getNextNode(myGraph); //uses a heuristic function to pick the next node to color in order to speed up processing
+
+                if (nextNode < nodeColors.length) //check that the next node actually exists
+                {
+                    simpleBTColor(nextNode, myGraph); //recursively attempt to color the next node
+
+                    //test if we successfully assigned the next node a color
+                    //if so, then the coloring we just tried for curNode is good and the color was found!
+                    if (nodeColors[nextNode] != colorVals[0]) foundColor = true;
+                }
+                else foundColor = true;
+            }
+
+            i++;
+        }
+
+        //if there wasn't a valid color for this node, set it back to uncolored (backtracking step)
+        if(!foundColor) nodeColors[curNode] = colorVals[0];
+    }
+    //*********End Code for Simple Backtracking**********
+    //***************************************************
+
+    //***************************************************
+    //*********Shared Backtracking Procedures************
+    private boolean causesConflicts(Graph myGraph, int theNode, int newColor) //SHARED BACKTRACKING PROC
+    {
+        ArrayList<Integer> neighborList = myGraph.getEdges(theNode);
+
+        for (Integer j : neighborList)
+        {
+            cost++;
+            if (newColor == nodeColors[j]) return true;
+        }
+
+        return false;
+    }
+
+    //Procedure to facilitate getting the next node to color, gets the next uncolored node with the max number of neighbors
+    //(heuristic is to color the node involved in the maximum number of constraints)
+    private int getNextNode(Graph myGraph) //SHARED BACKTRACKING PROC
+    {
+        int maxNeighbors = -1;
+        int nextNode = -1;
+
+        for (int i = 0; i <= nodeColors.length - 1; i++) //could speed this up by using a priority queue instead of a dumb linear search
+        {
+            cost++;
+            //test if node i is uncolored and has the most neighbors we've seen so far
+            if (nodeColors[i] == colorVals[0] && myGraph.getEdges(i).size() > maxNeighbors)
+            {
+                maxNeighbors = myGraph.getEdges(i).size();
+                nextNode = i;
+            }
+        }
+
+        //if nextNode was never assigned a new value >= 0 then all nodes have a color
+        //return an out-of-bounds value to indicate all nodes are colored and halt recursion
+        if (nextNode < 0) nextNode = nodeColors.length;
+        return nextNode;
+    }
+    //*********End Shared Backtracking Procedures********
+    //***************************************************
 }
