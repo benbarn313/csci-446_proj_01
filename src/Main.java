b069bd7384c55ea976f1d95212823e70f0513066
Ben P <polk.ben@gmail.com>
2020-01-28 23:26:42 -0700

    Add testing code in Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,20 @@ public class Main
 
     public static void main(String[] args)
     {
-	    Graph myGraph = new Graph(20);
+	    Graph myGraph = new Graph(100);
 	    if (DEBUG_LEVEL >= 1) System.out.print(myGraph);
+
+	    ConstraintSolver constraintSolver = new ConstraintSolver();
+        int numColors = 4;
+
+        constraintSolver.backtrackWithFwdCheck(myGraph, numColors);
+        System.out.println(constraintSolver.getColoring());
+        System.out.println(constraintSolver.getCost());
+
+	    constraintSolver.simpleBacktrack(myGraph, numColors);
+	    System.out.println(constraintSolver.getColoring());
+	    System.out.println(constraintSolver.getCost());
+
+
     }
 }
