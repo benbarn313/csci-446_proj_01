6bc3d56801fa2e1b281d657c23d31809b48f02fb
Ben P <polk.ben@gmail.com>
2020-01-30 23:58:01 -0700

    Remove testing code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 
     public static void main(String[] args)
     {
-	    Graph myGraph = new Graph(90);
+	    Graph myGraph = new Graph(20);
 	    if (DEBUG_LEVEL >= 1) System.out.print(myGraph);
 
 	    ConstraintSolver constraintSolver = new ConstraintSolver();
@@ -25,20 +25,5 @@ public class Main
         constraintSolver.localSearchGeneticAlgorithm(myGraph, numColors);
         System.out.println(constraintSolver.getColoring());
         System.out.println(constraintSolver.getCost());
-    }
-
-    private static void test()
-    {
-        int stuff[][] = new int[4][3];
-        int stuff2[] = {1, 2, 3};
-        int stuff3[] = {4, 5, 6};
-        stuff[1] = stuff2;
-        stuff[2] = stuff3;
-
-        for (int i = 0; i <= stuff[1].length - 1; i++) System.out.print(stuff[1][i]);
-        System.out.println();
-        for (int i = 0; i <= stuff[2].length - 1; i++) System.out.print(stuff[2][i]);
-        System.out.println();
-        System.out.println(stuff.length + " | " + stuff[1].length);
     }
 }
